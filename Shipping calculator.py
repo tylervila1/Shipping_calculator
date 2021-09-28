f87d333997d2769f963790b508f5a2ec7278e4a8
tylervila1 <86322241+tylervila1@users.noreply.github.com>
2021-09-28 14:21:18 -0600

    Update Shipping calculator.py

--- Shipping calculator.py
+++ Shipping calculator.py
@@ -1,7 +1,5 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup as soup
 # Shipping cost calculator
 
 
@@ -25,9 +23,9 @@ zip1 = zip_raw1
 zip2 = zip_raw2
 
 distance_ship_cost = .009
-miles = open("distance", "r+")
+#miles = open("distance", "r+")
 
-def distance_cost ():
+def cost ():
     driver = webdriver.Chrome()
     driver.get("https://www.zip-codes.com/distance_calculator.asp")
     sleep(2)
@@ -35,23 +33,22 @@ def distance_cost ():
     driver.find_element_by_id("to").send_keys(zip2)
     driver.find_element_by_name("submit").click()
     distance_raw = driver.find_element_by_class_name("mi").text
-    miles.write(distance_raw)
+    distance_raw2 = distance_raw.replace(',','')
+    miles = float(distance_raw2)
 
 
     driver.close()
     driver.quit()
 
-def total_cost ():
-    distance_value = miles.read()
-    test = int(distance_value)
+    distance_cost = (miles * distance_ship_cost)
     volume = (length * width * height)
     size_cost = (volume * volume_ship_cost)
     weight_cost = (weight * weight_ship_cost)
-    total_cost = (size_cost + weight_cost + test)
+    total_cost = (size_cost + weight_cost + distance_cost)
+    rounded_cost = round(total_cost)
 
-    print("$", total_cost)
+    print("$", rounded_cost)
+cost()
 
-distance_cost()
-total_cost()
 
 
